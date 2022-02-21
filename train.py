cde2bb29ad703f32698df3e8a1c44948a17db0f8
jcharis <jcharistech@gmail.com>
2022-02-21 22:21:18 +0200

    Modified Train.py with New Changes

--- train.py
+++ train.py
@@ -8,6 +8,17 @@ def greetings(name):
 def say_goodbye():
 	print("Good bye")
 
+
+def say_goodday():
+	print("Good Day")
+
+def train_everyday():
+	print("Train everyday and make changes")
+
+
 say_hello("John")
 greetings("John")
 say_goodbye()
+say_goodday()
+train_everyday()
+
