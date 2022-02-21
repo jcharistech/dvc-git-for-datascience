1d26b5caa2568a6f34ace3f153e45e783eaa729f
jcharis <jcharistech@gmail.com>
2022-02-21 13:40:18 +0200

    Modified Train.py with a new fxn

--- train.py
+++ train.py
@@ -2,4 +2,8 @@ def say_hello(name):
 	print("Hello {}, the Data Scientist ".format(name))
 
 
+def greetings(name):
+	print("This is a greeting from {}".format(name))
+
 say_hello("John")
+greetings("John")
