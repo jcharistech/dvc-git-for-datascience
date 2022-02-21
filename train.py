0ecd05a573c68f57f9fa733b329e0748f5e24c8d
jcharis <jcharistech@gmail.com>
2022-02-21 13:59:39 +0200

    2nd Modification of Train.py file

--- train.py
+++ train.py
@@ -5,5 +5,9 @@ def say_hello(name):
 def greetings(name):
 	print("This is a greeting from {}".format(name))
 
+def say_goodbye():
+	print("Good bye")
+
 say_hello("John")
 greetings("John")
+say_goodbye()
